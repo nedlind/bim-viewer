48da3794e7dc80f6637a34c793161ce4bd4ca674
Niklas Edlind <niklas.edlind@gmail.com>
2023-01-02 17:50:52 +0100

    restart

---
+++ app.js
@@ -0,0 +1,68 @@
+import { 
+    AmbientLight,
+    AxesHelper,
+    DirectionalLight,
+    GridHelper,
+    PerspectiveCamera,
+    Scene,
+    WebGLRenderer,
+ } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+// Create Three.js scene
+const scene = new Scene();
+const size = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+};
+
+// Create camera
+const camera = new PerspectiveCamera(75, size.width / size.height);
+camera.position.z = 15;
+camera.position.y = 13:
+camera.position.x = 8;
+
+// Create lights
+const lightColor = 0xffffff;
+const ambientLight = new AmbientLight(lightColor, 0.5);
+scene.add(ambientLight)
+
+const directionalLight = new DirectionalLight(lightColor, 2);
+directionalLight.position.set(0, 10, 0)
+scene.add(directionalLight);
+
+// Set up renderer
+const threeCanvas = document.getElementById('three-canvas');
+const renderer = new WebGLRenderer({ canvas: threeCanvas, alpha: true });
+renderer.setSize(size.width, size.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Create grid and axes
+const grid = new GridHelper(50, 30);
+scene.add(grid);
+
+const axes = new AxesHelper();
+axes.material.depthTest = false;
+axes.renderOrder = 1;
+scene.add(axes);
+
+const controls = new OrbitControls(camera, threeCanvas);
+controls.enableDamping = true;
+controls.target.set(-2, 0, 0);
+
+// Animation loop
+const animate = () => {
+    controls.update();
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+};
+
+animate();
+
+// Adjust viewport to the browser size
+window.addEventListener('resize', () => {
+    (size.width = window.innerWidth), (size.height = window.innerHeight);
+    camera.aspect = size.width / size.height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(size.width, size.height);
+});
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 
 export default {
-    input: 'index.js',
+    input: 'app.js',
     output: [
         {
             format: 'esm',
